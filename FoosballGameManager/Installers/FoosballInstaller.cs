a25cf998810f6122638c08cbb7bccabdb138b4e9
unknown <Uffe@.(none)>
2012-05-05 19:06:06 +0200

    More windsor

---
+++ FoosballGameManager/Installers/FoosballInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace FoosballGameManager.Installers
+{
+    public class FoosballInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromThisAssembly()
+                .Pick()
+                .LifestyleTransient());
+        }
+    }
+}
