de2de23777235f6db0853b7b68e36b9903d3f84c
unknown <Uffe@.(none)>
2012-05-20 17:16:11 +0200

    Register all Controllers in FoosballGameManagerInstaller

--- FoosballGameManager/Installers/FoosballGameManagerInstaller.cs
+++ FoosballGameManager/Installers/FoosballGameManagerInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Castle.MicroKernel.Registration;
+﻿using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 
@@ -8,9 +9,9 @@ namespace FoosballGameManager.Installers
 	{
 		public void Install(IWindsorContainer container, IConfigurationStore store)
 		{
-			container.Register(/*Classes.FromThisAssembly()
-				.Pick()
-				.LifestyleTransient()*/);
+			container.Register(Classes.FromThisAssembly()
+				.BasedOn<IController>()
+				.LifestyleTransient());
 		}
 	}
 }
