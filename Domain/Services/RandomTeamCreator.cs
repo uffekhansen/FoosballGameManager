e6946ece4ad4dbf9341ba5b84baf40bb318f6728
unknown <Uffe@.(none)>
2012-06-07 21:27:41 +0200

    Expect IRandom i RandomTeamCreator constructor

--- Domain/Services/RandomTeamCreator.cs
+++ Domain/Services/RandomTeamCreator.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Domain.Entities;
+using Domain.Tools;
 
 namespace Domain.Services
 {
 	public class RandomTeamCreator : TeamCreator
 	{
-		public RandomTeamCreator(int playersPerTeam, List<Player> players)
+		private IRandom _random;
+
+		public RandomTeamCreator(IRandom random, int playersPerTeam, List<Player> players)
 			: base(playersPerTeam, players)
 		{
+			_random = random;
 		}
 
 		protected override List<Team> GenerateTeams()
