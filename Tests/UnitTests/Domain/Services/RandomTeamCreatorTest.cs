e8bbe0f6b2afb4ecf28af611dbffdedc9f40fc4d
unknown <Uffe@.(none)>
2012-06-07 21:24:33 +0200

    Refactor tests

--- Tests/UnitTests/Domain/Services/RandomTeamCreatorTest.cs
+++ Tests/UnitTests/Domain/Services/RandomTeamCreatorTest.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Domain.Entities;
 using Domain.Services;
+using Domain.Tools;
 using Xunit;
 using Xunit.Extensions;
 
@@ -16,14 +17,19 @@ namespace Tests.UnitTests.Domain.Services
 		[InlineData(1, 2, 2)]
 		[InlineData(5, 5, 1)]
 		[InlineData(3, 6, 2)]
-		public void Given_randomTeamCreator_When_creating_teams_Then_the_expected_number_of_teams_are_returned(int expectedNumberOfTeams, int numberPlayersInlist, int playersPerTeam)
+		public void Given_randomTeamCreator_When_creating_teams_Then_the_expected_number_of_teams_are_returned(int expectedNumberOfTeams, int numberPlayersInlist, int numberPlayersPerTeam)
 		{
-			var playerList = ArrangePlayerList(numberPlayersInlist);
-			var teamCreator = new RandomTeamCreator(playersPerTeam, playerList);
+			var teamCreator = ArrangeTeamCreator(numberPlayersInlist, numberPlayersPerTeam);
 
 			var teams = teamCreator.CreateTeams();
 
 			Assert.Equal(expectedNumberOfTeams, teams.Count);
+		}
+
+		private RandomTeamCreator ArrangeTeamCreator(int numberPlayersInList, int numberPlayersPerTeam)
+		{
+			var playerList = ArrangePlayerList(numberPlayersInList);
+			return new RandomTeamCreator(new FoosBallRandom(), numberPlayersPerTeam, playerList);
 		}
 
 		private List<Player> ArrangePlayerList(int numberPlayers)
