96b39e6522aa0534914c854fd6a2726387dbea87
Uffe K. Hansen <ukh@mvno.dk>
2012-06-10 16:07:02 +0200

    Add TeamController

---
+++ FoosballGameManager/Controllers/TeamController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace FoosballGameManager.Controllers
+{
+    public class TeamController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
