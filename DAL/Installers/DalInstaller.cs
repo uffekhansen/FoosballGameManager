a82603da833fcd9a6e164a3fd9f08c44c1d55d8f
unknown <Uffe@.(none)>
2012-07-08 09:34:07 +0200

    Create DalInstaller

---
+++ DAL/Installers/DalInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace FoosballGameManager.Installers
+{
+	public class DalInstaller : IWindsorInstaller
+	{
+		public void Install(IWindsorContainer container, IConfigurationStore store)
+		{
+			container.Register(Classes.FromThisAssembly()
+				.InSameNamespaceAs<DalInstaller>()
+				.LifestyleTransient());
+		}
+	}
+}
