a6b9965d476932600ef7662c54e4ce6e058831b7
unknown <Uffe@.(none)>
2012-07-08 09:13:30 +0200

    Delete comments

--- Domain/Entities/Player.cs
+++ Domain/Entities/Player.cs
@@ -2,8 +2,7 @@
 {
 	public class Player
 	{
-		//TODO: Understanding of the benefit of converting to properties
 		public string Name;
-		public string Affiliation; //TODO: Naming... Importance: pre-set connection to a group of players that affect however the team is generated
+		public string Affiliation;
 	}
 }
