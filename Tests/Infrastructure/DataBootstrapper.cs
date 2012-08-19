e926086df53cb419b9f89bebbaa25a3216085152
unknown <Uffe@.(none)>
2012-08-19 19:03:58 +0200

    Add databootstrapper

---
+++ Tests/Infrastructure/DataBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Mappings;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace Tests.Infrastructure
+{
+	public class DataBootstrapper
+	{
+		private const string _databaseFilename = @"C:\Users\Uffe\Desktop\dbfiles\foosballTests.db";
+
+		public void Boostrap()
+		{
+			CreateSessionFactory();
+		}
+
+		private void CreateSessionFactory()
+		{
+			Fluently.Configure()
+				.Database(SQLiteConfiguration.Standard.UsingFile(_databaseFilename))
+				.Mappings(m => m.FluentMappings.Add<PlayerMapping>())
+				.BuildSessionFactory();
+		}
+	}
+}
