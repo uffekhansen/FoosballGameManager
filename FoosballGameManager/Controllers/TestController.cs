45f3457bbec8526f0a5b77411a81c11ad683f476
unknown <Uffe@.(none)>
2012-08-19 15:30:27 +0200

    Add test controller and view

---
+++ FoosballGameManager/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace FoosballGameManager.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
