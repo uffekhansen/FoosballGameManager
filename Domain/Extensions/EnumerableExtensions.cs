1e43a3df9f27294a552dd5414d98abc87d8ec62d
unknown <Uffe@.(none)>
2012-09-01 14:52:40 +0200

    Add Each() extension call

---
+++ Domain/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Extensions
+{
+	public static class EnumerableExtensions
+	{
+		public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
+		{
+			foreach (var item in enumerable)
+			{
+				action(item);
+			}
+		}
+	}
+}
---
+++ Tests/UnitTests/Domain/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.UnitTests.Domain.Extensions
+{
+	public class EnumerableExtensionsTest
+	{
+		[Fact]
+		public void Given_Action_And_Strings_When_Calling_Each_Then_Action_Is_Invoked_For_Each_Element()
+		{
+			IEnumerable<int> items = new List<int> { 1, 2, 3 };
+			var visited = new List<int>();
+			Action<int> action = visited.Add;
+
+			items.Each(action);
+
+			visited.Should().ContainInOrder(items);
+			visited.Should().HaveCount(items.Count());
+		}
+	}
+}
