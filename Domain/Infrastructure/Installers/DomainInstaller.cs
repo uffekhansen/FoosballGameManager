5a0aaa929743f5ebb6df752cef2ce6e282cc4d16
unknown <Uffe@.(none)>
2012-09-16 18:34:58 +0200

    usings

--- Domain/Infrastructure/Installers/DomainInstaller.cs
+++ Domain/Infrastructure/Installers/DomainInstaller.cs
@@ -1,7 +1,6 @@
 ﻿using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
-using Domain.Services;
 
 namespace Domain.Infrastructure.Installers
 {
