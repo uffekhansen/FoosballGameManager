12c865a99e3f76ea13d5533459843ffcc234a15e
Uffekhansen <ukh@mvno.dk>
2012-09-23 15:21:40 +0200

    Attempting add of player

--- FoosballGameManager/Controllers/PlayerController.cs
+++ FoosballGameManager/Controllers/PlayerController.cs
@@ -1,16 +1,21 @@
 ﻿using System.Web.Mvc;
 using DAL.Queries;
+using Domain.Entities;
 
 namespace FoosballGameManager.Controllers
 {
 	public class PlayerController : Controller
 	{
-		public PlayerController()
+		private readonly IAddPlayerQuery _addPlayerQuery;
+
+		public PlayerController(IAddPlayerQuery addPlayerQuery)
 		{
+			_addPlayerQuery = addPlayerQuery;
 		}
 
 		public ActionResult AddPlayer()
 		{
+			_addPlayerQuery.Execute(new Player());
 			return RedirectToAction("Index", "Team");
 		}
 	}
