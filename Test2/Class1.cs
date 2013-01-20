3d21220dfef906c2eb413c9fde4185b425e5ebbe
Uffekhansen <ukh@mvno.dk>
2013-01-20 21:29:46 +0100

    Added mock test project

---
+++ Test2/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Test2
+{
+    public class Class1
+    {
+        [Fact]
+        public void Is_Ok()
+        {
+            
+        }
+    }
+}
